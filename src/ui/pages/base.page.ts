a47c194ec2b9733d15b6b1a3d5a5ac60a836cd53
DiShurukhina <dolbunok@gmail.com>
2025-11-17 16:37:37 +0200

    API lecture code

--- src/ui/pages/base.page.ts
+++ src/ui/pages/base.page.ts
@@ -1,11 +1,12 @@
 import { expect, Locator, Page } from "@playwright/test";
 import { SALES_PORTAL_URL } from "config/env";
 
+
 export abstract class BasePage {
   abstract readonly uniqueElement: Locator;
   constructor(protected page: Page) {}
   async open() {
-    await this.page.goto(SALES_PORTAL_URL);
+    await this.page.goto(SALES_PORTAL_URL!);
   }
   async waitForOpened() {
     await expect(this.uniqueElement).toBeVisible();
---
+++ src/utils/schema.utils.ts
@@ -0,0 +1,16 @@
+import { expect } from "@playwright/test";
+import Ajv from "ajv";
+
+export function validateJsonSchema(body: object, schema: object) {
+  const ajv = new Ajv();
+  const validate = ajv.compile(schema);
+  const isValid = validate(body);
+  expect.soft(isValid, `Responce body should match JSON schema`).toBe(true);
+
+  if (isValid) {
+    console.log("Data is valid according to the schema.");
+  } else {
+    console.log("Data is not valid according to the schema.");
+    console.log(validate.errors);
+  }
+}
